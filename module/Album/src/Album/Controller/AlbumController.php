088d14cec68a941fc88a266a810ee070d71b277e
Jimmy Grand <alfirin.dagnir@gmail.com>
2013-04-22 11:01:40 -0500

    Create The model on the controller

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,24 +1,62 @@
 <?php
 namespace Album\Controller;
 
+use Album\Model\Album;
+use Album\Model\AlbumTable;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Class AlbumController
+ * @package Album\Controller
+ */
 class AlbumController extends AbstractActionController
 {
+    /**
+     * @var AlbumTable
+     */
+    protected $albumTable;
+
+    /**
+     * @return ViewModel
+     */
     public function indexAction()
     {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
     }
 
+    /**
+     *
+     */
     public function addAction()
     {
     }
 
+    /**
+     *
+     */
     public function editAction()
     {
     }
 
+    /**
+     *
+     */
     public function deleteAction()
     {
+    }
+
+    /**
+     * @return AlbumTable
+     */
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
     }
 }
